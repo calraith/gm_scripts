4ff681ef03930d8bb142639ba1e79133edfad15e
calraith <creepy@gmail.com>
2016-11-14 07:57:37 -0500

    Update se_grace_timer.user.js

--- se_grace_timer.user.js
+++ se_grace_timer.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        SE realtime dates
+// @name		SE realtime dates
 // @namespace   http://stackapps.com/
 // @description show Stack Exchange timestamps as realtime counters
 // @match		*://stackexchange.com/*
@@ -11,23 +11,23 @@
 // @match		*://superuser.com/*/*
 // @match		*://askubuntu.com/*/*
 // @match		*://mathoverflow.net/*/*
-// @version     1.2
+// @version	 1.2
 // @downloadURL	https://github.com/calraith/gm_scripts/raw/master/se_grace_timer.user.js
-// @grant       none
+// @grant	   none
 // ==/UserScript==
 
 function setIntervalWithContext(code, delay, context) {
-    return setInterval(function() {
-        code.call(context);
-    }, delay);
+	return setInterval(function() {
+		code.call(context);
+	}, delay);
 }
 
 function startTimer(el) {
-    if (!el.title) return;
-    this.el = el;
-    this.interval = setIntervalWithContext(function() {
-        if (!this.el) return clearInterval(this.interval);
-        var x = (new Date() - new Date(this.el.title.replace(' ','T'))) / 1000;
+	if (!el.title) return;
+	this.el = el;
+	this.interval = setIntervalWithContext(function() {
+		if (!this.el) return clearInterval(this.interval);
+		var x = (new Date() - new Date(this.el.title.replace(' ','T'))) / 1000;
 		if (x > -1) {
 			if (x>299&&x<360) this.el.innerHTML = '5 mins ago';
 			if (isNaN(x) || x > 299) return clearInterval(this.interval);
@@ -36,16 +36,16 @@ function startTimer(el) {
 				s = (s < 10) ? '0' + s : s;
 			this.el.innerHTML = m+':'+s+' ago';
 		}
-    }, 250, this);
-    timers.push(this.interval);
+	}, 250, this);
+	timers.push(this.interval);
 }
 
 function addTimers() {
-    while (timers.length) clearInterval(timers.pop());
-    var timestamps = document.getElementsByClassName('relativetime');
-    for (var i=0; i<timestamps.length; i++) {
-        var stopwatch = new startTimer(timestamps[i]);
-    }
+	while (timers.length) clearInterval(timers.pop());
+	var timestamps = document.getElementsByClassName('relativetime');
+	for (var i=0; i<timestamps.length; i++) {
+		var stopwatch = new startTimer(timestamps[i]);
+	}
 }
 
 var timers = [];
