4feca993731919b550167cd80b6e8e846570ef87
calraith <creepy@gmail.com>
2017-06-05 08:53:20 -0400

    Add files via upload
    Changelog v1.1: added Chrome + Tampermonkey compatibility

--- Alt+Num_Extended_Char_Entry.user.js
+++ Alt+Num_Extended_Char_Entry.user.js
@@ -1,49 +1,64 @@
-// ==UserScript==
-// @name			Alt+Num Extended Char Entry
-// @namespace		https://stackapps.org/
-// @description	hold Alt while pressing numbers to make extended characters
-// @match			*://*/*
-// @version		1
-// @grant			none
-// @run-at			document-idle
-// @downloadURL	https://github.com/calraith/gm_scripts/raw/master/Alt+Num_Extended_Char_Entry.user.js
+﻿// ==UserScript==
+// @name        Alt+Num Extended Char Entry
+// @namespace   https://stackapps.org/
+// @description hold Alt while pressing numbers to make extended characters
+// @match       *://*/*
+// @version	    1.1
+// @grant       none
+// @downloadURL https://github.com/calraith/gm_scripts/raw/master/Alt+Num_Extended_Char_Entry.user.js
+// @run-at      document-end
 // ==/UserScript==
 
 if (self != top) return;
 
 // holds a list of number keys pressed while alt is held down
-var combo = [];
+var combo = [],
+	clicked; // Chrome failsafe
 
-addEventListener('keyup', simulateNumPad);
+addEventListener('keyup', safeSimulateNumPad);
 addEventListener('load', iFramesToo);
 
 function iFramesToo() {
 	var iframes = document.getElementsByTagName('iframe');
 	for (var i=iframes.length; i--;) {
-		if (iframes[i].contentDocument)
-			iframes[i].contentDocument.addEventListener('keyup', simulateNumPad);
+		// try-catch block to avoid cross domain errors halting execution
+		try {
+			if (iframes[i].contentWindow) {
+				iframes[i].contentWindow.addEventListener('keyup', safeSimulateNumPad);
+				// Chrome failsafe
+				iframes[i].contentWindow.addEventListener('click', function(e) { clicked = e.target; });
+			}
+		}
+		catch (err) {}
 	}
+}
+
+function safeSimulateNumPad(e) {
+	try { simulateNumPad(e); }
+	catch(err) { console.log(err.message); }
 }
 
 function simulateNumPad(e) {
 
-	var el = getActiveElement(),
+	var el = getActiveElement() || clicked,
 		start = el.selectionStart,
 		end = el.selectionEnd,
 		symbol;
-	
+
 	if (!el) console.log('unable to determine focus');
 
-	if (!/textarea|input/i.test(el.nodeName) && !el.isContentEditable) return;
+	// Chrome doesn't reliably bubble up the .isContentEditable property
+	if (!/chrome/i.test(navigator.userAgent) && !/textarea|input/i.test(el.nodeName) &&
+		!el.isContentEditable) return;
 
 	if (e.altKey) {
 
 		// if alt key is pressed, prevent browser menubar from appearing
-		e.preventDefault();	
-		
+		e.preventDefault();
+
 		// keyCode 48 = 0, 49 = 1, 50 = 2, etc.
 		if (e.keyCode >= 48 && e.keyCode <= 57) combo.push(e.keyCode - 48);
-		
+
 	} else if (combo.length) {
 
 		// if key combo doesn't start with a zero, retrieve higher Unicode glyphs
@@ -52,22 +67,29 @@ function simulateNumPad(e) {
 			(combo[0]) ? combo.join('') * 1 + 703 : combo.join('')
 		);
 
+		console.log('symbol: ' + symbol);
+
 		// purge key sequence
 		combo = [];
 
 		// insert symbol at current caret pos and advance caret
 		if (el.setSelectionRange) {
-			
+
 			// textarea or input
 			el.value = el.value.substring(0, start) + symbol + el.value.substring(end);
 			el.setSelectionRange(++start, start);
-			
-		} else {
-			
-			// contentEditable node
-			var sel = el.ownerDocument.getSelection(),
-				range = sel.getRangeAt(0);
 
+		} else {
+
+			// contentEditable node
+			var sel = el.ownerDocument.defaultView.getSelection(),
+				range;
+
+			// hack for Chrome
+			if (!sel.rangeCount)
+				sel = clicked.ownerDocument.defaultView.getSelection();
+
+			range = sel.getRangeAt(0);
 			range.deleteContents();
 			range.insertNode(document.createTextNode(symbol));
 			sel.collapseToEnd();
@@ -82,17 +104,22 @@ function getActiveElement(document) {
 	var el = document.activeElement;
 
 	// if target is an iframe or is the document body with no slection
+	// (defaultView seems to be required for Chrome.)
 	if (el.tagName == 'IFRAME' ||
-		(el === document.body && !el.ownerDocument.getSelection())) {
-		
+		(el === document.body && !el.ownerDocument.defaultView.getSelection())) {
+
 		// enumerate all iframes and test recursively
 		var iframes = document.getElementsByTagName('iframe');
 		for (var i = iframes.length; i--;) {
 
-			if (iframes[i].contentDocument)
-				var focused = getActiveElement(iframes[i].contentDocument);
+			// try-catch to avoid cross domain errors halting execution
+			try {
+				if (iframes[i].contentDocument)
+					var focused = getActiveElement(iframes[i].contentDocument);
 
-			if (focused) return focused;
+				if (focused) return focused;
+			}
+			catch (err) {}
 		}
 	}
 	else return el;
